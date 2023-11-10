d763cf3518d741778a5e5adb5db916c0ddf30c37
kas11234567891010987654321 <145571455+kas11234567891010987654321@users.noreply.github.com>
2023-11-10 09:29:46 +0100

    Add files via upload

---
+++ allcards.php
@@ -0,0 +1,547 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Smitcards</title>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+
+.w3-redscale, .w3-grayscale {
+    filter: none;
+}
+
+.w3-right{
+margin-top: 5px;
+}
+
+element.style {
+    width: 97%;
+    margin-bottom: 5px;
+}
+
+.w3-blue-smit{
+background-color: #003cfe;
+}
+
+</style>
+
+</head>
+
+<body class="w3-content" style="max-width:500000000px">
+
+ 
+
+ 
+
+<!-- Navbar (sit on top) -->
+
+<div class="w3-centre">
+
+  <div class="w3-bar w3-white w3-card" id="myNavbar">
+
+    <a href="index.php"class="w3-left"> <img src="logo.png" style="width:25%"></a>
+
+
+
+    <div class="w3-right w3-hide-small">
+
+      <a href="index.php" class="w3-bar-item w3-button">HOME</a>
+
+      <a href="allcards.php" class="w3-bar-item w3-button"style="width:50%><i class="fa fa-th"></i> All CARDS</a>
+
+      
+      <a href="subschribe.php" class="w3-bar-item w3-button"style="width:10%><i class="fa fa-usd"></i> SUBSCRIBE</a>
+
+      <a href="contact.php" class="w3-bar-item w3-button"style="width:10%><i class="fa fa-envelope"></i> CONTACT</a>
+
+    </div>
+
+    <!-- Hide right-floated links on small screens and replace them with a menu icon -->
+
+ 
+
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+
+      <i class="fa fa-bars"></i>
+
+    </a>
+
+  </div>
+
+</div>
+
+<!-- !PAGE CONTENT! -->
+
+<div class="w3-main" style="margin-Right:100000px">
+
+ 
+
+  <!-- Push down content on small screens -->
+
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+
+
+
+
+
+
+    
+
+    </div>
+
+  </div>
+
+ 
+
+  <div class="w3-container w3-text-003cfe" id="jeans">
+
+    <p>8 items</p>
+
+  </div>
+
+ 
+
+<!-- Product grid -->
+
+  <div class="w3-row w3-grayscale">
+
+   <div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="playstation_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ps10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>PlayStation<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="playstation_100.png" style="width:100%">
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ps100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>PlayStation<br><b>€99.99</b></p>
+
+      </div>
+  </div>
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="xbox_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=xb10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Xbox<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="xbox_50.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=xb50"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Xbox<br><b>€49.99</b></p>
+
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="google_play_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=gp10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Google Play<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="google_play_100.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=gp100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Google Play<br><b>€99.99</b></p>
+
+      </div>
+
+    </div>
+
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="apple_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ap10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Apple<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="apple_100.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ap100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Apple<br><b>€99.99</b></p>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  
+  
+      <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+
+      <i class="fa fa-bars"></i>
+
+    </a>
+
+  </div>
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "ps10")
+{
+?>
+ <img src="playstation_10.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="ps100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "ps100")
+{
+?>
+ <img src="playstation_100.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="ps100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "xb10")
+{
+?>
+ <img src="xbox_10.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="xb100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+ 
+ 
+ 
+ 
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "xb50")
+{
+?>
+ <img src="xbox_50.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="xb100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "gp10")
+{
+?>
+ <img src="google_play_10.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="gp100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "gp100")
+{
+?>
+ <img src="google_play_100.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="gp100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "ap10")
+{
+?>
+ <img src="apple_10.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="ap10"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
+
+
+
+</div>
+<div>
+<?php
+$card = $_GET["card"];
+if($card == "ap100")
+{
+?>
+ <img src="apple_100.png" style="width:50%">
+ 
+
+
+
+
+<?php
+}elseif($card=="ap100"){
+?>
+
+
+
+<?php
+}
+
+?>
+
+</div>
