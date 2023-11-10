cbf45d721bf1279aa0457256c1e963e75cff87fb
kas11234567891010987654321 <145571455+kas11234567891010987654321@users.noreply.github.com>
2023-11-10 09:28:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,474 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Smitcards<title>
+<title>Smitcards</title>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+
+.w3-redscale, .w3-grayscale {
+    filter: none;
+}
+
+.w3-right{
+margin-top: 5px;
+}
+
+element.style {
+    width: 97%;
+    margin-bottom: 5px;
+}
+
+.w3-blue-smit{
+background-color: #003cfe;
+}
+
+</style>
+
+</head>
+
+<body class="w3-content" style="max-width:500000000px">
+
+ 
+
+ 
+
+<!-- Navbar (sit on top) -->
+
+<div class="w3-centre">
+
+  <div class="w3-bar w3-white w3-card" id="myNavbar">
+
+    <a href="#home"class="w3-left"> <img src="logo.png" style="width:25%"></a>
+
+
+
+    <div class="w3-right w3-hide-small">
+
+      <a href="index.php" class="w3-bar-item w3-button">HOME</a>
+
+      <a href="allcards.php" class="w3-bar-item w3-button"style="width:50%><i class="fa fa-th"></i> All CARDS</a>
+
+      
+
+      <a href="subschribe.php" class="w3-bar-item w3-button"style="width:10%><i class="fa fa-usd"></i> SUBSCRIBE</a>
+
+      <a href="contact.php" class="w3-bar-item w3-button"style="width:10%><i class="fa fa-envelope"></i> CONTACT</a>
+
+    </div>
+
+    <!-- Hide right-floated links on small screens and replace them with a menu icon -->
+
+ 
+
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+
+      <i class="fa fa-bars"></i>
+
+    </a>
+
+  </div>
+
+</div>
+
+<!-- !PAGE CONTENT! -->
+
+<div class="w3-main" style="margin-Right:100000px">
+
+ 
+
+  <!-- Push down content on small screens -->
+
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+
+
+
+
+
+
+    
+
+    </div>
+
+  </div>
+
+ 
+
+  <div class="w3-container w3-text-003cfe" id="jeans">
+
+    <p>8 items</p>
+
+  </div>
+
+ 
+
+  <!-- Product grid -->
+
+  <div class="w3-row w3-grayscale">
+
+   <div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="playstation_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ps10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>PlayStation<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="playstation_100.png" style="width:100%">
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ps100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>PlayStation<br><b>€99.99</b></p>
+
+      </div>
+  </div>
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="xbox_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=xb10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Xbox<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="xbox_50.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=xb50"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Xbox<br><b>€49.99</b></p>
+
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="google_play_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=gp10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Google Play<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="google_play_100.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=gp100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Google Play<br><b>€99.99</b></p>
+
+      </div>
+
+    </div>
+
+
+
+
+
+<div class="w3-col l3 s6">
+
+      <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="apple_10.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ap10"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Apple<br><b>€9.99</b></p>
+
+      </div>
+
+     <div class="w3-container">
+
+        <div class="w3-display-container">
+
+          <img src="apple_100.png" style="width:100%">
+
+         
+
+          <div class="w3-display-middle w3-display-hover">
+
+            <a href="cardsdetails.php?card=ap100"><button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button></a>
+
+          </div>
+
+        </div>
+
+        <p>Apple<br><b>€99.99</b></p>
+
+      </div>
+
+    </div>
+
+  </div>
+
+ 
+
+  <!-- Subscribe section -->
+
+  <div class="w3-container w3-blue-smit w3-padding-32">
+
+    <h1>Subscribe</h1>
+
+    <p>To get special offers and VIP treatment:</p>
+
+    <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:100%"></p>
+
+    <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
+
+  </div>
+
+
+  <!-- Footer -->
+
+  <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
+
+    <div class="w3-row-padding">
+
+      <div class="w3-col s4">
+
+        <h4>Contact</h4>
+
+        <p>Questions? Go ahead.</p>
+
+        <form action="/action_page.php" target="_blank">
+
+          <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
+
+          <p><input class="w3-input w3-border" type="text" placeholder="Email" name="Email" required></p>
+
+          <p><input class="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required></p>
+
+          <p><input class="w3-input w3-border" type="text" placeholder="Message" name="Message" required></p>
+
+          <button type="submit" class="w3-button w3-block w3-black">Send</button>
+
+        </form>
+
+      </div>
+
+ 
+
+      <div class="w3-col s4">
+
+        <h4>About</h4>
+
+        <p><a href="#">About us</a></p>
+
+        <p><a href="#">We're hiring</a></p>
+
+        <p><a href="#">Support</a></p>
+
+        <p><a href="#">Find store</a></p>
+
+        <p><a href="#">Shipment</a></p>
+
+        <p><a href="#">Payment</a></p>
+
+        <p><a href="#">Gift card</a></p>
+
+        <p><a href="#">Return</a></p>
+
+        <p><a href="#">Help</a></p>
+
+      </div>
+
+ 
+
+      <div class="w3-col s4 w3-justify">
+
+        <h4>Store</h4>
+
+        <p><i class="fa fa-fw fa-map-marker"></i>Smitcards</p>
+
+        <p><i class="fa fa-fw fa-phone"></i>+31 639029302</p>
+
+        <p><i class="fa fa-fw fa-envelope"></i> Smitcards@mail.com</p>
+
+        <h4>We accept</h4>
+
+      <p><i class="fa fa-fw fa-credit-card"></i> Ideal</p>
+
+        <p><i class="fa fa-fw fa-credit-card"></i> Credit Card</p>
+        <p><i class="fa fa-fw fa-credit-card"></i> Davi</p>
+
+        <br>
+
+        <i class="fa fa-facebook-official w3-hover-opacity w3-large"></i>
+
+        <i class="fa fa-instagram w3-hover-opacity w3-large"></i>
+
+        <i class="fa fa-snapchat w3-hover-opacity w3-large"></i>
+
+        <i class="fa fa-pinterest-p w3-hover-opacity w3-large"></i>
+
+        <i class="fa fa-twitter w3-hover-opacity w3-large"></i>
+
+        <i class="fa fa-linkedin w3-hover-opacity w3-large"></i>
+
+      </div>
+
+    </div>
+
+  </footer>
+
+ 
+
+
+ 
+
+  <!-- End page content -->
+
+</div>
+
+ 
+
+<!-- Newsletter Modal -->
+
+<div id="newsletter" class="w3-modal">
+
+  <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
+
+    <div class="w3-container w3-white w3-center">
+
+      <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
+
+      <h2 class="w3-wide">NEWSLETTER</h2>
+
+      <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
+
+      <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
+
+      <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('newsletter').style.display='none'">Subscribe</button>
+
+    </div>
+
+  </div>
+
+</div>
+
+ 
+
+ 
+
+
+
+</script>
+
+ 
+
+</body>
+
+</html>
